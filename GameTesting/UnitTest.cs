f577bc978ef7e04b7181708a487b6e29c819a224
dHartley13 <hartleydavid13@gmail.com>
2017-02-21 18:09:32 +0000

    21022017 attempt
    installed Moq but cannot use. states im missing assembly reference

---
+++ GameTesting/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace GameTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
