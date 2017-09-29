733e7556e0c24ea19799c3b6a5536825e37905f0
dHartley13 <hartleydavid13@gmail.com>
2017-09-29 09:24:32 +0100

    Start of exporter

---
+++ MyPerformacePal/dataExporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace MyPerformacePal
+{
+    class dataExporter
+    {
+        private void endGameExporter(int GameID)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyPerformancePal;Integrated Security=True";
+            using (var dbConnection = new SqlConnection(connectionString))
+            {
+                dbConnection.Open();
+                var sqlCommand = new SqlCommand(, dbConnection);
+                
+            }
+        }
+    }
+}
